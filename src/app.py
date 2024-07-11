fe6a389bb53363b24f2e3d3227363dadcdfedbf6
pringletom <169591098+pringletom@users.noreply.github.com>
2024-07-11 11:08:14 +0100

    cannot run

--- src/app.py
+++ src/app.py
@@ -7,27 +7,29 @@ app = Flask(__name__)
 def test():
     return render_template("firstpage.j2")
 
-
 @app.route("/")
-def index():
-    user_name = session.get('user_name')
-    return render_template("index.html", user_name=user_name)
+def landingPage():
+    return render_template("landingpage.j2")
+# #@app.route("/")
+# #def index():
+#     user_name = session.get('user_name')
+#     return render_template("index.html", user_name=user_name)
 
 
-@app.route("/login", methods=["GET"])
-def login_landing():
-    return render_template("login.html")
+# @app.route("/login", methods=["GET"])
+# def login_landing():
+#     return render_template("login.html")
 
 
-@app.route("/login", methods=["POST"])
-def login_action():
-    email = request.form.get("email")
-    password = request.form.get("password")
-    if email == "admin@admin.com":
-        if password == "password123":
-            session['user_name'] = "admin"
-            return redirect('/')
-        else:
-            redirect('login.j2')
-    else:
-        redirect('login.j2')
+# @app.route("/login", methods=["POST"])
+# def login_action():
+#     email = request.form.get("email")
+#     password = request.form.get("password")
+#     if email == "admin@admin.com":
+#         if password == "password123":
+#             session['user_name'] = "admin"
+#             return redirect('/')
+#         else:
+#             redirect('login.j2')
+#     else:
+#         redirect('login.j2')
